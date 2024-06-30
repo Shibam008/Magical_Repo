e43bb752f447181a2bb416f592f682d3f61d9578
Shibam Sadhukhan <sadhukhan799@gmail.com>
2024-06-30 12:36:48 +0530

    func py

---
+++ PYTHON/functions/01_squareNum.py
@@ -0,0 +1,6 @@
+def square(n):
+   return n ** 2
+
+num = int(input("Enter a number : "))
+ans = square(num)
+print("Answer = ",ans)
---
+++ PYTHON/functions/polymorphism_in_func.py
@@ -0,0 +1,8 @@
+
+#* Write a function (multiply) that multiplies two numbers, but can also accept and multiply strings 
+
+def multiply(p1, p2):
+   return p1 * p2
+
+print(multiply(8, 5))
+print(multiply('s', 5))
